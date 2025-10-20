2958438ca4d560858835e948cdb70d890d967df6
ELROKA02 <samuelstonecaballero@gmail.com>
2025-10-20 17:16:31 +0200

    Prueba

--- Scripts.js
+++ Scripts.js
@@ -97,5 +97,13 @@ function Ej11(){
 }
 
 function Ej12(){
+    /**
+    * Ejercicio 12: Escriba una función que reciba dos arrays y devuelva un nuevo array con
+    los elementos que solo aparecen una vez en total (ya sea en el primer o en el segundo
+    array). El orden debe ser: primero los que están en el primer array y luego los que están
+    en el segundo.
+    Ejemplos:
+    [1, 2, 3, 3] y [3, 2, 1, 4, 5]) ==> [4, 5]
+    */
 
 }
